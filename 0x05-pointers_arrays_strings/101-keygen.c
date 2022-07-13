d05fc16e9e90bef380b196cd31d1bede43fd7bcd
Akanbi Opeyemi Johnson <106748430+LOVEWIX@users.noreply.github.com>
2022-07-14 00:13:18 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,37 +1,42 @@
-#include <time.h>
-#include <stdlib.h>
 #include <stdio.h>
-#define stopLimit 2645
-#define asciiStop 127
-#define asciiStart 32
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - generates a random password
- *
- * Return: 0 if success
- *
+ * main - random password generator for 101-crackme
+ * Author: Chimezie Onwudiwe
+ * Return: always 0
  */
-
 int main(void)
 {
-	char password[100];
-	int randValue, num = 0, i = 0;
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
 	srand(time(NULL));
-
-	while (num < stopLimit)
+	while (s != 2772)
 	{
-		randValue = random() % asciiStop;
-		if (randValue > asciiStart)
+		i = k = s = 0;
+		while ((2772 - 122) > s)
 		{
-			password[i++] = randValue;
-			num += randValue;
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
 		}
 	}
-
-	password[i++] = (2772 - num);
-	password[i] = '\0';
-	printf("%s", password);
-
+	p[i] = '\0';
+	printf("%s", p);
 	return (0);
 }
