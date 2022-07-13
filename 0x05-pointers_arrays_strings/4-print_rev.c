eac324e21de5f1ddc67e30c7d9bf5fa2d4ce5df7
Akanbi Opeyemi Johnson <106748430+LOVEWIX@users.noreply.github.com>
2022-07-14 00:15:07 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,22 @@
-#include <string.h>
 #include "main.h"
-
-void print_rev(char *str)
+/**
+ * print_rev -prints a string in reverse via putchar
+ * @s: a string to print in reverse
+ * Author: Onwudiwe Chimezie
+ * Return: a void
+ */
+void print_rev(char *s)
 {
-	int i, len = strlen(str);
+	int i = 0;
 
-	for (i = len - 1; i >= 0; i--)
-		_putchar(str[i]);
-
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
