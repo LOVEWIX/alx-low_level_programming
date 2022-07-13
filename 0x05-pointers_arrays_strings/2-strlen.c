9932d5ad68cb1fe93e2fabe341b2a2bc3e087aae
Akanbi Opeyemi Johnson <106748430+LOVEWIX@users.noreply.github.com>
2022-07-14 00:13:56 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,15 @@
-#include <string.h>
 #include "main.h"
-
+/**
+  * _strlen - give the length of a string
+  * @s: the string passed as a char ptr
+  * Author: Chimezie Onwudiwe
+  * Return: the length of a string
+  */
 int _strlen(char *s)
 {
-	return (strlen(s));
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+	return (i);
 }
